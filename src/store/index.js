e03ce16aa5d0c3847d88d59bc20a316cccee7c75
佛系玩家沈公子 <techpang666@gmail.com>
2021-04-03 08:59:08 +0800

    config vuex

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+// 安装配置vuex
+const store = new Vuex.Store({
+  // 开启严格模式
+  strict: true,
+  state: {
+    // 数据源
+  },
+  mutations: {
+    // 同步操作
+  },
+  actions: {
+    // 异步操作
+  },
+  getters: {
+    // store的计算属性
+  },
+  modules: {
+    // 模块化
+  }
+})
+
+export default store
---
+++ src/views/vuex_test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      这是Vuex的面试题专栏
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
